cb48c3184dfa695d194566be94ba33eb0083af25
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-02-13 11:18:48 +0100

    add solution for lab5

---
+++ courses/coding-in-C/Lab_5/mySolution/lab5_mySolution.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+  for(int i = 0; i<argc; i++){
+    printf("Argument %d at adress \"%p\" has content:\n%s (lenght: %d)\n", i, &argv[i], argv[i], strlen(argv[i]));
+  }
+  
+  return 0;
+}
