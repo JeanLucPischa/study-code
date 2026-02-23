bae1b48b0b16619d26b15a9f7171534f333c4165
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-02-23 14:00:11 +0100

    Add solution for Section 2 of Lab 7

---
+++ courses/coding-in-C/Lab_7/mySolution/Lab7_Sec2.c
@@ -0,0 +1,45 @@
+/*
+ *  File: Lab7_Sec2.c
+ *  Description:  
+ */
+
+#include <stdio.h>
+
+typedef struct node {
+  double value;
+  Node *ptr_next;
+} Node;
+
+typedef struct list {
+  Node *head;
+} List;
+
+Node *create_node(double value);
+
+int main(){
+
+  List list1 = {NULL};
+  List *ptr_list1 = &list1;
+  Node *temp_ptr = NULL;
+
+  for(int i=0; i<50; i++){
+    if(ptr_list1->head == NULL){
+      
+    }
+    else{
+      create_node(i)->ptr_next = temp_ptr;
+    }
+  }
+
+
+  return 0;
+}
+
+Node *create_node(double value){
+  Node this_node;
+  Node *ptr_this_node = &this_node;
+  ptr_this_node->value = value;
+  ptr_this_node->ptr_next = NULL;
+
+  return ptr_this_node;
+}
