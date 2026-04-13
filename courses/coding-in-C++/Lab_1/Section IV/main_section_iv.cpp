3fcaa44765e898bd28e1cfab34c7234101e6abb3
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-04-13 13:53:08 +0200

    continue working on section iv

--- courses/coding-in-C++/Lab_1/Section IV/main_section_iv.cpp
+++ courses/coding-in-C++/Lab_1/Section IV/main_section_iv.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    
+    std::string name;
+    std::uint8_t homework, midterm, final_exam;
+    readStudentData(name, homework, midterm, final_exam);
+
+    std::cout << name << std::endl << homework << std::endl << midterm << std::endl << final_exam << std::endl;
 
     return 0;
 }
