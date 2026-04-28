cddceebcde044cc31131248ef0d2e83f33ee2159
Jean-Luc Pischa <pischa.jean-lu-it25@it.dhbw-ravensburg.de>
2026-04-28 12:38:58 +0200

    finish lab 3 section ii

--- courses/coding-in-C++/Lab_3/section_ii/section_ii.cpp
+++ courses/coding-in-C++/Lab_3/section_ii/section_ii.cpp
@@ -6,22 +6,22 @@ class User
 {
     protected:
         std::string name;
-        std::uint8_t id;
+        std::uint16_t id;
 
     public:
         User() {};
-        User(std::string name, std::uint8_t id) : name(name), id(id) {}
+        User(std::string name, std::uint16_t id) : name(name), id(id) {}
         void printfInfo();
 };
 
 class Student : public User
 {
     private:
-        std::uint8_t active_classes;
-        std::uint8_t mat_number;
+        std::uint16_t active_classes;
 
     public:
-        Student(std::string name, std::uint8_t id, std::uint8_t active_classes, std::uint8_t mat_number) : User(name, id), active_classes(active_classes), mat_number(mat_number) {};
+        Student(std::string name, std::uint16_t id, std::uint8_t active_classes) : User(name, id), active_classes(active_classes) {};
+        void printRole();
 };
 
 class Instructor : public User
@@ -30,7 +30,8 @@ class Instructor : public User
         std::uint8_t object_age;
 
     public:
-        Instructor(std::string name, std::uint8_t id, std::uint8_t object_age) : User(name, id), object_age(object_age) {};
+        Instructor(std::string name, std::uint16_t id, std::uint8_t object_age) : User(name, id), object_age(object_age) {};
+        void printRole();
 };
 
 void User::printfInfo()
@@ -38,4 +39,39 @@ void User::printfInfo()
     std::cout << "Name: " << name << std::endl;
     std::cout << "ID: " << id << std::endl;
     return;
+}
+
+void Student::printRole()
+{
+    std::cout << "Student" << std::endl;
+    return;
+}
+
+void Instructor::printRole()
+{
+    std::cout << "Instructor" << std::endl << std::endl;
+    return;
+}
+
+//########################################################################################################################
+
+int main()
+{
+    Student greg("greg", 9823, 7);
+    Student sophie("sophie", 1934, 6);
+
+    Instructor schmidt("schmidt", 7412, 53);
+    Instructor  mueller("mueller", 1046, 32);
+
+    greg.printfInfo();
+    greg.printRole();
+    sophie.printfInfo();
+    sophie.printRole();
+
+    schmidt.printfInfo();
+    schmidt.printRole();
+    mueller.printfInfo();
+    mueller.printRole();
+
+    return 0;
 }
